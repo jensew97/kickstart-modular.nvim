d8bb2f6aba550fff517e47284195117922a086d6
Jens <jens.walmsness@maritimerobotics.com>
2024-04-03 08:39:19 +0200

    Add lualine and noice

--- init.lua
+++ init.lua
@@ -6,9 +6,6 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Set to true if you have a Nerd Font installed
-vim.g.have_nerd_font = true
-
 -- [[ Setting options ]]
 require 'options'
 
--- lua/custom/plugins/plugins.lua
+++ lua/custom/plugins/plugins.lua
@@ -4,16 +4,4 @@ return {
   { 'folke/zen-mode.nvim' },
 
   { 'mbbill/undotree' },
-
-  {
-    'NeogitOrg/neogit',
-    dependencies = {
-      'nvim-lua/plenary.nvim', -- required
-      'sindrets/diffview.nvim', -- optional - Diff integration
-
-      -- Only one of these is needed, not both.
-      'nvim-telescope/telescope.nvim', -- optional
-    },
-    config = true,
-  },
 }
