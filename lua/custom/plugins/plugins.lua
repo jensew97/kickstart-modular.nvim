d45292828eeff3bb1b9ee8c4279906a05dffe1b8
jens.walmsness <jens.walmsness@maritimerobotics.com>
2024-04-08 16:07:14 +0200

    Change width of zenmode

--- lua/custom/plugins/plugins.lua
+++ lua/custom/plugins/plugins.lua
@@ -1,7 +1,14 @@
 return {
   { 'christoomey/vim-tmux-navigator' },
 
-  { 'folke/zen-mode.nvim' },
+  {
+    'folke/zen-mode.nvim',
+    opts = {
+      window = {
+        width = 140,
+      },
+    },
+  },
 
   { 'mbbill/undotree' },
 }
